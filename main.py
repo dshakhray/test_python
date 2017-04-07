b0d80002c2d90aa1e0ac420ded472094ad8e7cd3
Darya Malyavkina <dmalyavkina@cloudlinux.com>
2017-04-07 14:02:02 +0400

    Add main

--- main.py
+++ main.py
@@ -1,37 +1,27 @@
 #!/usr/lib/env python
 # -*- mode:python; coding:utf-8; -*-
-# author: Darya Malyavkina <dmalyavkina@cloudlinux.com>
+# log_inor: Darya Malyavkina <dmalyavkina@cloudlinux.com>
 # created: 2017-03-30
 # description:
 
-from selenium import webdriver
-import time
 import git
-import os
 import logging
+import os
+import sys
+import time
 
-KERNEL_VER = 'kernel-rt-3.8.13-rt14.20.el6rt'#'kernel-3.10.0-514.el7'
+from selenium import webdriver
+
+KERNEL_VER = sys.argv[1]
 URL_PATCHES = 'https://access.redhat.com/labs/psb/versions/{}/patches/%s?raw=true'.format(KERNEL_VER)
 URL_PATCHES_LIST = 'https://access.redhat.com/labs/psb/versions/{}/patches'.format(KERNEL_VER)
 REMOTE_URL = 'git@github.com:dshakhray/test_python.git'
-asd = 'kernel-rt-3.8.13-rt14.20.el6rt'
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
 
-def error_handler(fn):
-    def wrapper(*args, **kwargs):
-        #logger = logging.getLogger(__name__)
-        try:
-            return fn(*args, **kwargs)
-        except Exception as e:
-            #logger.error("errrr")
-            wrapper(*args, **kwargs)
-    return wrapper
-
-
-@error_handler
-def auth():
-    if True:
-        raise IOError
+def log_in():
     driver = webdriver.PhantomJS()
     driver.get("https://www.redhat.com/wapps/sso/login.html")
     time.sleep(3)
@@ -39,12 +29,12 @@ def auth():
     driver.find_element_by_id("password").send_keys("05Anivis+2592")
     driver.find_element_by_id("_eventId_submit").click()
     time.sleep(3)
-    print 'auth'
+    logger.info('log_inorization succeeded in your account')
     return driver
 
 
-def download_patches():
-    driver = auth()
+def get_list_of_patches_names():
+    driver = log_in()
     driver.get(URL_PATCHES_LIST)
     time.sleep(10)
     patches_list = []
@@ -58,9 +48,10 @@ def download_patches():
         for i in range(count_click):
             driver.find_element_by_id("psb-load-more-patches").click()
             time.sleep(3)
-
     except Exception:
         pass
+
+    logger.info('A full list of patches has opened')
 
     table_id = driver.find_element_by_class_name("table-files")
     rows = table_id.find_elements_by_tag_name("tr")
@@ -70,17 +61,21 @@ def download_patches():
         if patch_name != '' and patch_name != "Name":
             patches_list.append(patch_name)
 
-    print 'create list of name'
+    logger.info('Created a list of patches names')
     return patches_list
 
 
-def create_files(patches_list=None):
-
+def download_patches_from_redhat():
     if not os.path.exists(KERNEL_VER):
         os.makedirs(KERNEL_VER)
+        patches_list = get_list_of_patches_names()
+    else:
+        patches_list = get_update_patches_names()
 
-    patches_list = patches_list or download_patches()
-    driver = auth()
+    driver = log_in()
+    counter_created_file = 0
+
+    logger.info('Started create files')
 
     for patch_name in patches_list:
         url = URL_PATCHES % patch_name
@@ -89,7 +84,8 @@ def create_files(patches_list=None):
         patch = driver.find_elements_by_tag_name("body")[0].text
 
         if patch == '':
-            driver = auth()
+            logger.info('Required re-authorize on the Red Hat account')
+            driver = log_in()
             driver.get(url)
             patch = driver.find_elements_by_tag_name("body")[0].text
 
@@ -97,17 +93,24 @@ def create_files(patches_list=None):
         f.write(patch.encode('utf-8'))
         f.close()
 
-    print 'create files'
+        counter_created_file += 1
+        if not counter_created_file % 50:
+            msg = 'Created {} files. It remains to process {} files'.format(counter_created_file,
+                                                                            len(patches_list) - counter_created_file)
+            logger.info(msg)
 
 
-def update_files():
-    patches_list = download_patches()
+def get_update_patches_names():
+    patches_list = get_list_of_patches_names()
     patches_update = []
 
     for patch_name in patches_list:
         patch_path = '%s/%s' % (KERNEL_VER, patch_name)
         if not os.path.exists(patch_path):
             patches_update.append(patch_name)
+
+    logger.info('A list of new patches has been created. '
+                'You need to add {} patches'.format(len(patches_update)))
     return patches_update
 
 
@@ -120,38 +123,23 @@ def git_init():
         origin = repo.remotes.origin
 
     origin.pull('master')
-    print 'git_init'
+    logger.info('Initialized Git repository. Repo is updated.')
 
 
-def git_branch():
+def push_changes():
     repo = git.Repo.init()
-    br = repo.create_head(KERNEL_VER)
-    repo.head.reference = br
     origin = repo.remotes.origin
 
     repo.index.add([KERNEL_VER])
     repo.index.commit("Add patches for %s" % KERNEL_VER)
-    origin.push(KERNEL_VER)
-    print 'git push %s' % KERNEL_VER
+    origin.push('master')
+    logger.info('Patches added to %s' % KERNEL_VER)
 
 
-def new_kernel():
+def main():
     git_init()
-    create_files()
-    #update_files()
-    git_branch()
-
-
-def update_kernel():
-    git_init()
-    patches_list = update_files()
-    create_files(patches_list)
-    git_branch()
+    download_patches_from_redhat()
+    push_changes()
 
 if __name__ == '__main__':
-    #git_init()
-    #create_files()
-    #update_files()
-    #git_push()
-    #git_branch()
-    auth()
+    main()
